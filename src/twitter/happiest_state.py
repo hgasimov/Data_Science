679dc4216eab4b2f1fb74b6273b73c9ae23f2a2a
hgasimov <gasimov.huseyn@gmail.com>
2013-06-03 12:23:08 -0700

    Which state is happiest?

---
+++ src/twitter/happiest_state.py
@@ -0,0 +1,47 @@
+import sys
+import json
+
+DEBUG = False
+
+def readScores(sent_file):
+    scores = {}
+    for line in sent_file:
+        term, score = line.split('\t')
+        scores[term] = int(score)
+    return scores
+
+def main():
+    maxstate = ''
+    maxscore = float("-inf")
+    
+    sent_file = open(sys.argv[1])        
+    scores = readScores(sent_file)
+    
+    tweet_file = open(sys.argv[2])
+    for tweet in tweet_file.readlines():
+        jtweet = json.loads(tweet)
+        if 'text' in jtweet:
+            score = calcScore(jtweet['text'], scores)
+            if score > maxscore:
+                username = jtweet['user']['name'].strip()
+                maxstate = username[0] + username[-1]
+                maxscore = score 
+                if DEBUG: print "%s : %s %f" % (username, maxstate, score)
+    
+    if DEBUG: print '\nMax state is %s with score %f' %(maxstate, maxscore)
+    print maxstate
+        
+
+def calcScore(s, scores):    
+    n = .0 # score
+    words = s.strip().split(' ')
+    for word in words:
+        if word in scores:
+            n+= scores[word]
+    return n
+
+    
+
+if __name__ == '__main__':
+    main()
+
